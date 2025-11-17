3c722575c3a8006738ff4ff5c5d577030c33bd8b
Imtiaj Ahmad <imtiaj2207092@stud.kuet.ac.bd>
2025-11-18 05:09:26 +0600

    preview controller

--- src/main/java/com/example/cv_builder/PreviewController.java
+++ src/main/java/com/example/cv_builder/PreviewController.java
@@ -1,8 +1,37 @@
 package com.example.cv_builder;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class PreviewController {
 
+    @FXML private ImageView photoView;
+    @FXML private Label nameText;
+    @FXML private Label emailText;
+    @FXML private Label phoneText;
+    @FXML private Label addressText;
+    @FXML private Label educationText;
+    @FXML private Label skillsText;
+    @FXML private Label experienceText;
+    @FXML private Label projectsText;
 
+    public void loadData(CVModel m) {
+        nameText.setText("Name: " + m.getName());
+        emailText.setText("Email: " + m.getEmail());
+        phoneText.setText("Phone: " + m.getPhone());
+        addressText.setText("Address: " + m.getAddress());
+        educationText.setText(m.getEducation());
+        skillsText.setText(m.getSkills());
+        experienceText.setText(m.getExperience());
+        projectsText.setText(m.getProjects());
+
+
+        if (m.getPhotoPath() != null && !m.getPhotoPath().isEmpty()) {
+            Image image = new Image("file:" + m.getPhotoPath());
+            photoView.setImage(image);
+        }
+    }
 }
 
