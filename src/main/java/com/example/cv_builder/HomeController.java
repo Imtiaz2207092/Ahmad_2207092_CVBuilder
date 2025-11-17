a8efa2c9af92bfe392a0ba786635ee821155d3a9
Imtiaj Ahmad <imtiaj2207092@stud.kuet.ac.bd>
2025-11-18 04:22:05 +0600

    homecontroller setup

--- src/main/java/com/example/cv_builder/HomeController.java
+++ src/main/java/com/example/cv_builder/HomeController.java
@@ -1,9 +1,30 @@
 package com.example.cv_builder;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import java.io.IOException;
 
 public class HomeController {
 
+    @FXML
+    private void openCreateCV(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("CreateCV.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
 
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.setTitle("Create Your CV");
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
-
